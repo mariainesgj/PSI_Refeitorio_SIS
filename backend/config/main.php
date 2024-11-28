3d6f443984db0ec9fab6444666564d9516026d7e
Maria Inês <2211923@my.ipleiria.pt>
2024-11-28 12:30:46 +0000

    feat: login and register

--- backend/config/main.php
+++ backend/config/main.php
@@ -46,16 +46,21 @@ return [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/user'],
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/cozinha'],
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/ementa'],
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/fatura'],
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/linhascarrinho'],
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/linhasfatura'],
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/movimento'],
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/profile'],
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/senha'],
-                ['class' => 'yii\rest\UrlRule','controller' => 'api/valor'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/user',
+                    'extraPatterns' => [
+                        'POST login' => 'login' ,
+                        'POST register' => 'register'
+                    ]
+                ],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/cozinha'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/ementa'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/fatura'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/linhascarrinho'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/linhasfatura'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/movimento'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/profile'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/senha'],
+                ['class' => 'yii\rest\UrlRule', 'controller' => 'api/valor'],
 
             ],
         ],
--- backend/modules/api/controllers/UserController.php
+++ backend/modules/api/controllers/UserController.php
@@ -2,7 +2,11 @@
 
 namespace backend\modules\api\controllers;
 
+use common\models\Profile;
+use common\models\User;
+use Yii;
 use yii\rest\ActiveController;
+use yii\web\BadRequestHttpException;
 
 /**
  * Default controller for the `api` module
@@ -18,4 +22,111 @@ class UserController extends ActiveController
     {
         return $this->render('index');
     }
+
+    public function auth($username, $password)
+    {
+        $user = \common\models\User::findByUsername($username);
+        if ($user && $user->validatePassword($password))
+        {
+            $this->user=$user;
+            return $user;
+        }
+        throw new \yii\web\ForbiddenHttpException('Incorect username or password'); //Error 403
+    }
+
+    public function actionLogin()
+    {
+        $rawBody = Yii::$app->request->getRawBody();
+        $request = json_decode($rawBody, true);
+
+        $username = $request['username'] ?? null;
+        $password = $request['password'] ?? null;
+        $user = $this->auth($username, $password);
+
+        return [
+            'status' => 'success',
+            'message' => 'Login bem-sucedido!',
+            'username' => $username,
+            'access_token' => $user->getAuthKey(),
+            'token_type' => 'bearer',
+        ];
+    }
+
+
+
+    public function actionRegister()
+    {
+        try {
+            $rawBody = Yii::$app->request->getRawBody();
+            $requestData = json_decode($rawBody, true);
+
+            if (empty($requestData['user']) || empty($requestData['profile'])) {
+                throw new BadRequestHttpException('Dados de usuário ou perfil estão ausentes.');
+            }
+
+            $user = $requestData['user'];
+            $username = $user['username'] ?? null;
+            $email = $user['email'] ?? null;
+            $password = $user['password'] ?? null;
+
+            $profile = $requestData['profile'];
+            $name = $profile['name'] ?? null;
+            $mobile = $profile['mobile'] ?? null;
+            $street = $profile['street'] ?? null;
+            $locale = $profile['locale'] ?? null;
+            $postalCode = $profile['postalCode'] ?? null;
+            $role = $profile['role'] ?? null;
+            $cozinha_id = $profile['cozinha_id'] ?? null;
+
+            if (!$username || !$email || !$password) {
+                throw new BadRequestHttpException('Campos obrigatórios (username, email, password) estão faltando.');
+            }
+
+            $userModel = new User([
+                'username' => $username,
+                'email' => $email,
+                'password' => Yii::$app->security->generatePasswordHash($password),
+                'status' => 10,
+                'auth_key' => Yii::$app->security->generateRandomString(),
+                'verification_token' => Yii::$app->security->generateRandomString(),
+            ]);
+
+            if (!$userModel->validate() || !$userModel->save()) {
+                throw new BadRequestHttpException('Erro ao criar o usuário. Verifique se o nome de usuário ou email já estão em uso.');
+            }
+
+            $profileModel = new Profile([
+                'name' => $name,
+                'mobile' => $mobile,
+                'street' => $street,
+                'locale' => $locale,
+                'postalCode' => $postalCode,
+                'role' => $role,
+                'user_id' => $userModel->id,
+                'cozinha_id' => $cozinha_id
+            ]);
+
+            if (!$profileModel->validate() || !$profileModel->save()) {
+                throw new BadRequestHttpException('Falha ao criar o perfil.');
+            }
+
+            Yii::$app->response->statusCode = 200;  // HTTP status code 200
+            return [
+                'status' => 'success',
+                'message' => 'Usuário criado com sucesso!',
+                'data' => [
+                    'user' => $userModel->attributes,
+                    'profile' => $profileModel->attributes,
+                ],
+            ];
+        } catch (\Exception $e) {
+            Yii::$app->response->statusCode = 500;  // HTTP status code 500
+            return [
+                'status' => 'error',
+                'message' => $e->getMessage(),
+            ];
+        }
+    }
+
+
 }
